0aa06976f82fe7d4ec72a28bb2479b5cf7496c9e
Mateusz Żukowski <74957@student.pb.edu.pl>
2022-01-24 23:08:26 +0100

    Add project files.

---
+++ RGBIterator/Iterator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace RGBIterator
+{
+    class Iterator
+    {
+        private short _step = 51; // works the best with divisors of 255 (1, 3, 5, 15, 17, 51, 85, 255); lower step = smoother changes
+
+        private List<int> _RGB;
+
+        private static Iterator _instance = new Iterator();
+
+        private Iterator()
+        {
+            _RGB = new List<int> { 255, 0, 0 };
+        }
+
+        public static Iterator getIterator()
+        {
+            return _instance;
+        }
+
+        public Color Next()
+        {
+            for(byte i = 0; i < 3; i++)
+            {
+                if (_RGB[i] == 0)
+                {
+                    if (_RGB[(i + 1) % 3] == 255)
+                    {
+                        if (_RGB[(i + 2) % 3] != 255)
+                        {
+                            increase((i + 2) % 3);
+                            break;
+                        }
+                        else
+                        {
+                            reduce((i + 1) % 3);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (_RGB[(i + 1) % 3] != 0)
+                        {
+                            reduce((i + 1) % 3);
+                            break;
+                        }
+                        else
+                        {
+                            increase(i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return Color.FromRgb((byte)_RGB[0], (byte)_RGB[1], (byte)_RGB[2]);
+        }
+
+        private void reduce(int index)
+        {
+            _RGB[index] = _RGB[index] - _step;
+
+            if (_RGB[index] < 0) _RGB[index] = 0;
+        }
+
+        private void increase(int index)
+        {
+            _RGB[index] = _RGB[index] + _step;
+
+            if (_RGB[index] > 255) _RGB[index] = 255;
+        }
+
+        public Boolean isDone()
+        {
+            return false; // it never ends
+        }
+    }
+}
---
+++ RGBIterator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RGBIterator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Iterator iterator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            iterator = Iterator.getIterator();
+        }
+
+        private void MainGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainGrid.Background = new SolidColorBrush(iterator.Next());
+        }
+    }
+}
